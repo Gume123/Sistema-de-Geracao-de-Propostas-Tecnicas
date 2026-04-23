88ec8601f3b405648c2b6acc23d74753673363a7
Guilherme Costa <162715969+Gume123@users.noreply.github.com>
2026-04-23 09:47:39 -0300

    lalalala

--- app/config.py
+++ app/config.py
@@ -9,3 +9,9 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60")
 
 USUARIO = os.getenv("USUARIO", "admin")
 SENHA = os.getenv("SENHA", "123456")
+
+# Render passa a variável DATABASE_URL automaticamente com 'postgres://'
+# Porém, o SQLAlchemy 1.4+ requer que seja 'postgresql://'
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sistema.db")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
